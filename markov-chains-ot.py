51903f296eab8ca01b3e1a37a37de64bc7d4e67c
Olivia Tran <oliviatran99@gmail.com>
2022-04-03 15:25:16 -0700

    formatting paragraphs

--- markov-chains-ot.py
+++ markov-chains-ot.py
@@ -12,7 +12,7 @@ text_model = markovify.Text(text)
 short_story = ''
 
 while len(short_story.split(" ")) < 1000:
-    for i in range(random.randrange(3, 9)):
+    for i in range(random.randrange(4, 9)):
         short_story += text_model.make_sentence() + " "
     short_story += '\n\n'
 
